faa9fb0d62d4d1e170d315102580c3561ea0cb6c
Himanshu Khandelwal <himanshukhandelwal@Himanshus-MacBook-Pro.local>
2024-04-28 11:28:22 +0530

    title changed

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Home_V5 from "./(home)/home-v5/page";
 import Wrapper from "./layout-wrapper/wrapper";
 
 export const metadata = {
-  title: "Home v1 || Homez - Real Estate NextJS Template",
+  title: "Prop Cliq",
 };
 
 export default function MainRoot() {
